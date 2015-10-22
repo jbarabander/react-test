963ca9294fb92de2154e80bccd34a834a3832379
Justin Barabander <jbarabander@gmail.com>
2015-10-22 13:05:45 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res, next) {
+    res.sendFile(path.join(__dirname, '../index.html'));
+})
+
+
+var server = app.listen(3000, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('app listening on port 3000');
+})
