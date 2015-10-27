b4eb2582f0d141b22963be2e03e2c880cd4dcfb7
Justin Barabander <jbarabander@gmail.com>
2015-10-27 17:51:14 -0400

    Started models

---
+++ server/db/models/index.js
@@ -0,0 +1,3 @@
+require('./location.js');
+require('./query.js');
+require('./user.js');
---
+++ server/env/development.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "DATABASE_URI": "mongodb://localhost:27017/intermeetiate",
+  "SESSION_SECRET": "Optimus Prime is my real dad",
+  "GITHUB": {
+    "clientID": "4cbe9744a30f1a07032b",
+    "clientSecret": "e3143e0522ff0ac5e62e9097b1e576acf2a45abe",
+    "callbackURL": "http://127.0.0.1:1337/auth/github/callback"
+  },
+  "FACEBOOK": {
+    "clientID": "513618282137958",
+    "clientSecret": "98949149e3c8c7eeddf74df7e040a5d7",
+    "callbackURL": "http://127.0.0.1:1337/auth/facebook/callback"
+  },
+  "GOOGLE": {
+    "clientID": "18279746287-aed0t6rr4fc0t12onn7ljfqitei8k1fj.apps.googleusercontent.com",
+    "clientSecret": "5u9ZkFypACCp9scCeFnTPNLB",
+    "callbackURL": "http://127.0.0.1:1337/auth/google/callback"
+  }
+};
