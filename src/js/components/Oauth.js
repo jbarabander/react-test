8a00b6e2b80bbb83976eb0c329a3167cca5ebc92
Justin Barabander <jbarabander@gmail.com>
2015-10-29 19:08:15 -0400

    Beautifying login form

--- src/js/components/Oauth.js
+++ src/js/components/Oauth.js
@@ -2,6 +2,16 @@ var React = require('react');
 
 module.exports = React.createClass({
     render: function() {
-
+        return (
+            <div className='oauth-group text-center'>
+                <h3>Or...do this thing</h3>
+                <div className='icon-container'>
+                    <div className='col-sm-6 col-xs-12'><a href='#'><i className="fa fa-github fa-l"></i></a></div>
+                    <div className='col-sm-6 col-xs-12'><a href='#'><i className="fa fa-google-plus fa-l"></i></a></div>
+                    <div className='col-sm-6 col-xs-12'><a href='#'><i className="fa fa-facebook fa-l"></i></a></div>
+                    <div className='col-sm-6 col-xs-12'><a href='#'><i className="fa fa-twitter fa-l"></i></a></div>
+                </div>
+            </div>
+        )
     }
 })
--- src/js/components/SignUp.js
+++ src/js/components/SignUp.js
@@ -1,75 +1,19 @@
 var React = require('react');
-var UserActions = require('../actions/UserActions.js');
-var formElements = [
-    {stateName: 'firstName', name: 'first name', type: 'text', formClass: 'half-form-el'},
-    {stateName: 'lastName', name: 'last name', type: 'text', formClass: 'half-form-el'},
-    {stateName: 'email', name:'email', type: 'email', formClass: 'full-form-el'},
-    {stateName: 'password', name:'password', type: 'password', formClass: 'half-form-el'},
-    {stateName: 'repeatedPassword', name: 'repeated password', type: 'password', formClass: 'half-form-el'},
-    {stateName: 'username', name: 'username', type: 'text', formClass: 'full-form-el'}
-];
+var OAuth = require('./Oauth.js');
+var LocalSignUp = require('./LocalSignUp');
 
 module.exports = React.createClass({
-    getInitialState: function() {
-        return {
-            firstName: '',
-            lastName: '',
-            email: '',
-            password: '',
-            repeatedPassword: '',
-            username: ''
-        };
-    },
-    handleChange: function(type, event) {
-        var state = {};
-        state[type] = event.target.value;
-        this.setState(state);
-    },
     render: function() {
-        //<h1>--Or use one of these-- </h1>
-        var values = this.state;
-        var self = this;
         return (
             <div className='container'>
-                <h1>Create an account</h1>
+                <h1>Sign Up</h1>
                 <div className='signup-container'>
                     <div className='flex-container'>
-                        <div className='local-signup-group'>
-                            <form className='form-group'>
-                                {formElements.map(function(element, index) {
-                                    return (
-                                        <div key={index} className={element.formClass}>
-                                            <label>{element.name}</label>
-                                            <input className='form-control' type={element.type} value={values[element.stateName]}
-                                                   onChange={self.handleChange.bind(self, element.stateName)}/>
-                                        </div>
-                                    )
-                                })}
-                                <button className='btn btn-info view submit-btn' onClick={this.submitUser}>Submit</button>
-                            </form>
-                        </div>
-                        <div className='oauth-group text-center'>
-                            <div className='icon-container'>
-                                <div className='col-sm-6 col-xs-12'><a href='#'><i className="fa fa-github fa-l"></i></a></div>
-                                <div className='col-sm-6 col-xs-12'><a href='#'><i className="fa fa-google-plus fa-l"></i></a></div>
-                                <div className='col-sm-6 col-xs-12'><a href='#'><i className="fa fa-facebook fa-l"></i></a></div>
-                                <div className='col-sm-6 col-xs-12'><a href='#'><i className="fa fa-twitter fa-l"></i></a></div>
-                            </div>
-                        </div>
-
+                        <LocalSignUp/>
+                        <OAuth/>
                     </div>
-
                 </div>
-
             </div>
-
-
         )
-    },
-    submitUser: function() {
-        if(this.state.password === this.state.repeatedPassword) {
-            console.log('hi');
-            UserActions.create(this.state);
-        }
     }
 });
