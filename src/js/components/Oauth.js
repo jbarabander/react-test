b39f01bb80b2fbf640f39e706cd3a8474c58b7f5
Justin Barabander <jbarabander@gmail.com>
2015-10-29 22:27:32 -0400

    Made it mobile responsive

--- src/js/components/Oauth.js
+++ src/js/components/Oauth.js
@@ -3,13 +3,13 @@ var React = require('react');
 module.exports = React.createClass({
     render: function() {
         return (
-            <div className='oauth-group text-center'>
-                <h3>Or...do this thing</h3>
+            <div className='oauth-group'>
+                <h3 className='oauth-title'>Or use these guys</h3>
                 <div className='icon-container'>
-                    <div className='col-sm-6 col-xs-12'><a href='#'><i className="fa fa-github fa-l"></i></a></div>
-                    <div className='col-sm-6 col-xs-12'><a href='#'><i className="fa fa-google-plus fa-l"></i></a></div>
-                    <div className='col-sm-6 col-xs-12'><a href='#'><i className="fa fa-facebook fa-l"></i></a></div>
-                    <div className='col-sm-6 col-xs-12'><a href='#'><i className="fa fa-twitter fa-l"></i></a></div>
+                    <div className='col-md-6 col-sm-3 col-xs-6'><a href='#' className='icon-link'><i className="fa fa-github fa-l"></i></a></div>
+                    <div className='col-md-6 col-sm-3 col-xs-6'><a href='#' className='icon-link'><i className="fa fa-google-plus fa-l"></i></a></div>
+                    <div className='col-md-6 col-sm-3 col-xs-6'><a href='#' className='icon-link'><i className="fa fa-facebook fa-l"></i></a></div>
+                    <div className='col-md-6 col-sm-3 col-xs-6'><a href='#' className='icon-link'><i className="fa fa-twitter fa-l"></i></a></div>
                 </div>
             </div>
         )
