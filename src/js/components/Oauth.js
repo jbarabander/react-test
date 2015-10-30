806200b1f02ed0632a29dddf9979d24f765b3420
Justin Barabander <jbarabander@gmail.com>
2015-10-29 23:20:08 -0400

    Some more styling

--- src/js/components/Oauth.js
+++ src/js/components/Oauth.js
@@ -4,7 +4,7 @@ module.exports = React.createClass({
     render: function() {
         return (
             <div className='oauth-group'>
-                <h3 className='oauth-title'>Or use these guys</h3>
+                <h3 className='oauth-title'>Or login through</h3>
                 <div className='icon-container'>
                     <div className='col-md-6 col-sm-3 col-xs-6'><a href='#' className='icon-link'><i className="fa fa-github fa-l"></i></a></div>
                     <div className='col-md-6 col-sm-3 col-xs-6'><a href='#' className='icon-link'><i className="fa fa-google-plus fa-l"></i></a></div>
