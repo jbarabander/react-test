441ebaaa542122c77795b11c735b1a77758bbbea
Justin Barabander <jbarabander@gmail.com>
2015-11-03 03:49:45 -0500

    Finished prelim styling of navbar

--- src/js/components/Contact.js
+++ src/js/components/Contact.js
@@ -3,8 +3,16 @@ var React = require('react');
 module.exports = React.createClass({
     render() {
         return (
-            <div>
-
+            <div className='container'>
+                <h2>Leave us Feedback</h2>
+                <form className='form-group'>
+                    <div className='form-inline'>
+                        <label>First name</label>
+                        <input type="text" className='form-control'/>
+                        <label>Last name</label>
+                        <input type="text" className='form-control'/>
+                    </div>
+                </form>
             </div>
         )
     }
