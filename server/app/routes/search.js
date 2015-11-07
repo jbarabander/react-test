07880b899e1f0712d694c1928559f91dbe4cbeac
Justin Barabander <jbarabander@gmail.com>
2015-11-07 00:44:12 -0500

    Continued with search_route

--- server/app/routes/search.js
+++ server/app/routes/search.js
@@ -1,19 +1,26 @@
 var router = require('express').Router();
 var mongoose = require('mongoose');
+var Promise = require('bluebird');
+var User = mongoose.model('User');
 
+function searchQuerySpecify(obj, type) {
+    return !obj.type || obj.type === type;
+}
 
 router.get('/', function(req, res, next) {
     console.log('search');
-    if(req.query.type) {
-        return next();
+    var obj = {};
+    if(searchQuerySpecify(req.query, 'user')) {
+        obj.users = User.findByUsernameOrEmail(req.query.q)
+    }
+    if(searchQuerySpecify(req.query, 'location')) {
     }
 
-}, function(req, res, next) {
-    if(req.query.type === 'user') {
-        res.redirect('/api/users');
-    } else {
-        res.redirect('/api/locations');
-    }
-}); //FIXME: Possibly???
+    Promise.props(obj)
+    .then(function(obj) {
+            console.log(obj);
+            res.json(obj);
+        })
 
+});
 module.exports = router;
