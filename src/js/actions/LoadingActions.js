ff4c80cc0d427cbba503398ba8407bb61ab1b6da
Justin Barabander <jbarabander@gmail.com>
2015-11-16 10:48:42 -0500

    moved loading logic to seperate actions file

--- src/js/actions/LoadingActions.js
+++ src/js/actions/LoadingActions.js
@@ -1,2 +1,16 @@
-var AppDispatcher = require('../dispatchers/AppDispatcher.js');
-var loadingConstants = require('../constants/loadingConstants.js');
+let AppDispatcher = require('../dispatchers/AppDispatcher.js');
+let loadingConstants = require('../constants/loadingConstants.js');
+
+let startLoading = () => {
+    AppDispatcher.handleViewAction({
+        actionType: loadingConstants.LOADING_STARTED
+    });
+};
+let stopLoading = () => {
+    AppDispatcher.handleViewAction({
+        actionType: loadingConstants.LOADING_STOPPED
+    })
+};
+
+module.exports = {startLoading, stopLoading};
+
--- src/js/actions/SearchActions.js
+++ src/js/actions/SearchActions.js
@@ -1,13 +1,15 @@
 var AppDispatcher = require('../dispatchers/AppDispatcher.js');
 var request = require('superagent');
 var searchConstants = require('../constants/searchConstants.js');
+var LoadingActions = require('../actions/LoadingActions.js');
 
 var search = (content) => {
-    console.log(content);
+    LoadingActions.startLoading();
     request.get('api/search')
-    .query(content)
-    .end((err, res) => {
-            if(err) throw new Error(err.message);
+        .query(content)
+        .end((err, res) => {
+            if (err) throw new Error(err.message);
+            LoadingActions.stopLoading();
             AppDispatcher.handleViewAction({
                 actionType: searchConstants.SEARCH_LAUNCH,
                 content: JSON.parse(res.text)
