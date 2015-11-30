caae6dff0d9b7e0300be66871b5042a648bf4427
Justin Barabander <jbarabander@gmail.com>
2015-11-29 22:22:38 -0500

    Finished basic propRouter setup

--- src/js/propRouter/index.js
+++ src/js/propRouter/index.js
@@ -1,10 +1,10 @@
-var cloneWithProps = require('react').cloneElement;
+var cloneElement = require('react').cloneElement;
 
 module.exports = function(element, state) {
     var elementToReturn = element;
     switch(element.props.route.path) {
         case '/users/:id':
-            elementToReturn = cloneWithProps(element, {foundUser: state.currentUser});
+            elementToReturn = cloneElement(element, {foundUser: state.currentUser});
             break;
     }
     return elementToReturn;
